b40f9652ca60788299ecbacde2dc20733204dfc1
Jim Kirkbride <james.kirkbride@gmail.com>
2014-09-28 18:00:30 -0400

    Accepting nickels.

--- features/VendingMachine.feature
+++ features/VendingMachine.feature
@@ -2,3 +2,7 @@ Feature: Accept Coins
 	Scenario: No coins inserted
 	Given no coins are inserted
 	Then the vending machine displays 'INSERT COIN'
+
+	Scenario: Accept a nickel
+	Given a nickel is inserted
+	Then the vending machine displays '0.05'
--- features/step_definitions/VendingMachineSteps.cpp
+++ features/step_definitions/VendingMachineSteps.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include <cucumber-cpp/defs.hpp>
 
+using namespace VendingMachineApp;
 using cucumber::ScenarioScope;
 
 struct Context {
@@ -12,8 +13,14 @@ WHEN("^no coins are inserted$") {
     //Do nothing
 }
 
+GIVEN("^a nickel is inserted$") {
+    ScenarioScope<Context> context;
+    context->vendingMachine.insert(NICKEL);
+}
+
 
 THEN("^the vending machine displays '(.*)'$") {
     ScenarioScope<Context> context;
-    EXPECT_EQ("INSERT COIN", context->vendingMachine.readDisplay());
+    REGEX_PARAM(std::string, displayValue);
+    EXPECT_EQ(displayValue, context->vendingMachine.readDisplay());
 }
